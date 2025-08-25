74e77034068901988c2aeb8e4340a1d43bce01da
wuliya <3369906077@qq.com>
2025-08-25 21:43:57 +0800

    chore: 初始化仓库

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import * as puppeteer from 'puppeteer-core'
+
+export const {
+  /**
+   * @public
+   */
+  connect,
+  /**
+   * @public
+   */
+  defaultArgs,
+  /**
+   * @public
+   */
+  executablePath,
+  /**
+   * @public
+   */
+  launch,
+} = puppeteer
+
+export default puppeteer
+export * from 'puppeteer-core'
